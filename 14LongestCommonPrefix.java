725feecba6da77652c071e51ef5e9037b332acb4
QIU JINGYI <amber.jingyiqiu@gmail.com>
2018-06-26 11:57:34 +1200

    6.26

---
+++ 14LongestCommonPrefix.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+
+class 14-Longest-Common-Prefix {
+
+    public static void main (String[] args) {
+      String[] strs = {"flower","flow","flight"};
+      Arrays.sort(strs);
+      for(String s:strs){
+        System.out.println(s);
+      }
+    }
+
+}
